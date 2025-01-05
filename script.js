79e39f86358f9b480332f75618781859100873c7
PolyMath <2024EB02502@online.bits-pilani.ac.in>
2025-01-05 21:48:07 +0530

    Brush unfunctioned and not collabrative

---
+++ script.js
@@ -0,0 +1,78 @@
+const canvas = document.querySelector('canvas')
+ctx = canvas.getContext('2d')
+let isDrawing = false
+
+window.addEventListener('load', () => {
+    canvas.width = canvas.offsetWidth
+    canvas.height = canvas.offsetHeight
+})
+
+const startDraw = () =>{
+    isDrawing = true;
+    ctx.beginPath()
+    ctx.lineWidth = 3
+}
+const startErase = () => {
+    isDrawing = true;
+    ctx.beginPath()
+    ctx.lineWidth = 50
+    ctx.strokeStyle = 'white'
+}
+
+const drawing = (e) => {
+    if(!isDrawing) return
+    ctx.lineTo(e.offsetX, e.offsetY)
+    ctx.stroke()
+}
+
+canvas.addEventListener('mousemove', drawing);
+canvas.addEventListener('mouseup', () => isDrawing = false);
+
+let tol = document.querySelectorAll('.tl')
+
+tol.forEach( i => i.addEventListener('click', (e) => {
+        tol.forEach(i => {
+            i.style.boxShadow = '';
+            i.style.transform = '';
+        })
+        canvas.removeEventListener('mousedown', startDraw);
+        canvas.removeEventListener('mousedown', startErase);
+        i.style.boxShadow = `0 0 10px green`;
+        i.style.transform = 'scale(1.3)';
+        if(i.classList.contains('pen')){
+            canvas.addEventListener('mousedown', startDraw);
+            ctx.strokeStyle = 'black'
+        }else if(i.classList.contains('eraser')){
+            canvas.addEventListener('mousedown', startErase);
+        }else{
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            i.style.boxShadow = '';
+            i.style.transform = '';
+        }
+}))
+
+const pen = document.querySelector('.pen')
+const prp = document.querySelector('.abv')
+
+pen.addEventListener('dblclick', e => {
+    prp.classList.toggle('disp')
+})
+
+const inp = document.querySelectorAll('.inp')
+inp.forEach(i => {
+    i.addEventListener('blur', e =>{
+        prp.classList.toggle('disp')
+        if(i.classList.contains('clrbx')){
+            ctx.strokeStyle = i.value
+        }
+    })
+})
+
+const clr = document.querySelectorAll('.clr')
+clr.forEach(i => {
+    i.addEventListener('click', e =>{
+        ctx.strokeStyle = i.dataset.colour
+        prp.classList.toggle('disp')
+
+    })
+})
